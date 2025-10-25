73d7792c634ec153caeea34586dc31663be3b722
minabayat-bit <minabayatt9@gmail.com>
2025-10-25 20:21:20 +0330

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,10 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className=" h-full flex flex-col gap-6 justify-center items-center ">
-      <>
-        <h2 className="text-[50px] text-gray-500 font-bold ">Counter</h2>
-      </>
-      <>
-        <p
-          className={
-            count > 0
-              ? "text-green-600 text-8xl font-semibold"
-              : count < 0
-              ? "text-red-600 text-8xl font-semibold"
-              : "text-black text-8xl font-semibold"
-          }
-        >
-          {count}
-        </p>
-      </>
+    <div>
+      <h1>hello</h1>
     </div>
+    
   );
 }
 
